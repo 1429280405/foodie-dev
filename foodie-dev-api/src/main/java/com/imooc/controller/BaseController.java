ee45f9315845da16ef4c664ca8b2f9199ec8d8f5
liujinqiang <1429280405@qq.com>
2021-10-20 21:57:44 +0800

    新增用户登录、退出。整合logback日志，开启service日志监控及mybatis日志打印。

--- foodie-dev-api/src/main/java/com/imooc/controller/BaseController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/BaseController.java
@@ -3,6 +3,11 @@ package com.imooc.controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +17,7 @@ import java.util.Map;
  * @create 2021-10-14 10:30
  */
 public class BaseController {
-
+    public static final Integer COOKIE_DELETE = 0;
 
     public Map<String, String> getErrors(BindingResult result) {
         Map<String, String> map = new HashMap<>();
@@ -27,4 +32,36 @@ public class BaseController {
         return map;
     }
 
+
+    public void setCookie(HttpServletRequest request,
+                          HttpServletResponse response,
+                          String cookieName,
+                          String cookieValue,
+                          Integer maxAge) {
+        try {
+            cookieValue = URLEncoder.encode(cookieValue, "UTF-8");
+            setCookieValue(request, response, cookieName, cookieValue, maxAge);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setCookieValue(HttpServletRequest request, HttpServletResponse response, String cookieName, String cookieValue, Integer maxAge) {
+        Cookie cookie = new Cookie(cookieName, cookieValue);
+        cookie.setMaxAge(maxAge);
+//        cookie.setDomain("foodie-dev.com");
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
+
+    public void deleteCookie(HttpServletRequest request,
+                             HttpServletResponse response,
+                             String cookieName) {
+        try {
+            String deleteValue = URLEncoder.encode("", "utf-8");
+            setCookieValue(request, response, cookieName, deleteValue, COOKIE_DELETE);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
@@ -1,18 +1,24 @@
 package com.imooc.controller;
 
+import com.google.gson.Gson;
 import com.imooc.pojo.Users;
 import com.imooc.pojo.bo.UserBO;
 import com.imooc.service.UserService;
+import com.imooc.utils.CookieUtil;
 import com.imooc.utils.IMOOCJSONResult;
+import com.imooc.utils.JsonUtils;
 import com.imooc.utils.MD5Utils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 
@@ -20,10 +26,13 @@ import javax.validation.constraints.NotBlank;
  * @author liujq
  * @create 2021-10-13 9:41
  */
+@Slf4j
 @Api(value = "注册登录", tags = {"用户注册登录接口"})
 @RestController
 @RequestMapping("/passport")
 public class PassportController extends BaseController {
+
+    private static final Integer EXPIRE = 7200;
 
     @Autowired
     private UserService userService;
@@ -43,7 +52,7 @@ public class PassportController extends BaseController {
 
     @ApiOperation(value = "用户注册", notes = "用户注册", httpMethod = "POST")
     @PostMapping("/regist")
-    public IMOOCJSONResult regist(@RequestBody UserBO userBO) {
+    public IMOOCJSONResult regist(@RequestBody UserBO userBO, HttpServletRequest request, HttpServletResponse response) {
         String username = userBO.getUsername();
         String password = userBO.getPassword();
         String confirmPassword = userBO.getConfirmPassword();
@@ -62,13 +71,15 @@ public class PassportController extends BaseController {
             return IMOOCJSONResult.errorMsg("用户已存在，注册失败！");
         }
         //入库
-        userService.createUser(userBO);
+        Users users = userService.createUser(userBO);
+        users = setNullProperties(users);
+        setCookie(request, response, "user", JsonUtils.objectToJson(users), EXPIRE);
         return IMOOCJSONResult.ok();
     }
 
-    @ApiOperation(value = "用户登录", notes = "用户登录", httpMethod = "GET")
+    @ApiOperation(value = "用户登录", notes = "用户登录", httpMethod = "POST")
     @PostMapping("/login")
-    public IMOOCJSONResult login(@RequestBody UserBO userBO) {
+    public IMOOCJSONResult login(@RequestBody UserBO userBO, HttpServletRequest request, HttpServletResponse response) {
         String username = userBO.getUsername();
         String password = userBO.getPassword();
         if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
@@ -80,6 +91,26 @@ public class PassportController extends BaseController {
         if (users == null) {
             return IMOOCJSONResult.errorMsg("用户名密码不正确！");
         }
+        users = setNullProperties(users);
+        setCookie(request, response, "user", JsonUtils.objectToJson(users), EXPIRE);
         return IMOOCJSONResult.ok();
+    }
+
+    @ApiOperation(value = "用户退出", notes = "用户退出", httpMethod = "POST")
+    @PostMapping("/logout")
+    public IMOOCJSONResult logout(String userId, HttpServletRequest request, HttpServletResponse response) {
+        deleteCookie(request, response, "user");
+        return IMOOCJSONResult.ok();
+    }
+
+
+    public Users setNullProperties(Users users) {
+        users.setPassword(null);
+        users.setMobile(null);
+        users.setEmail(null);
+        users.setCreatedTime(null);
+        users.setUpdatedTime(null);
+        users.setBirthday(null);
+        return users;
     }
 }
